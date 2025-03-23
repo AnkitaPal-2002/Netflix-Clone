05985eb180321de01c18cf4942675130ff022f4c
AnkitaPal-2002 <ankitapal6112002@gmail.com>
2025-03-23 20:51:28 +0530

    add session management, JWT token generation, and environment variable configuration

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 
 dotenv.config();
 
@@ -27,6 +30,27 @@ app.use('/api/v1/auth', authRoutes);
 import { User } from './src/models/user.model.js';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
+app.use(session({
+    secret: 'abcd1234',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongoUrl: ENV_VARS.MONGO_URI}),
+    cookie:{
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}))
+
+// import passport from 'passport';
+// import { Strategy as LocalStrategy } from 'passport-local';
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true}));
+
 
 app.listen(PORT,()=>{
     console.log('Server is running on port',PORT);
